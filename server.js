817da0ea909cd7ddcce56e0c83d9770dce64fed6
SriramJothiswaran <connect2sriram@hotmail.com>
2017-04-08 20:29:51 +0530

    2nd commit

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'/public');
 const port = process.env.PORT || 3000;
 var app=express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 app.use(express.static(publicPath));
 
+io.on('connection',function(socket){
+   console.log('new user connected');
 
-app.listen(port,function(){
+    socket.on('disconnect',function(){
+       console.log('User was disconnected');
+    });
+});
+
+
+server.listen(port,function(){
     console.log('Server started at ' + port);
 });
