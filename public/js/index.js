cc2570877af79daaada0c2fea27dc693a646f927
Sriram Jothiswaran <sriram@Srirams-MacBook-Air.local>
2017-04-15 22:57:20 +0530

    made changes

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket = io();
+socket.on('connect', function(){
+    console.log("connected to server");
+    socket.emit("createMessage", {
+       from: "adrew@admin.com",
+        text: 'hello !!'
+    });
+});
+socket.on('disconnect', function(){
+    console.log("disconnected from server");
+});
+
+socket.on("newMessage", function(message){
+   console.log('New Email', message);
+});
+
--- server/server.js
+++ server/server.js
@@ -1,14 +1,44 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 app.use(express.static(publicPath));
 
+io.on('connection', function(socket){
+    console.log("user connected");
 
-app.listen(port, function(){
-    console.log('Server is up on port');
+    socket.emit('newEmail', {
+        from :'admin@admin.com',
+        text : 'hey! i m using socket',
+        to : 'user@user.com',
+        createdAt : new Date().getTime()
+    });
+
+    socket.emit('newMessage',{
+       from: 'John',
+        createdAt: '1234',
+        text: "See you then"
+    });
+
+    socket.on('createMessage', function(message){
+        console('message');
+    });
+
+
+    socket.on("disconnect", function(){
+       console.log("user disconnected");
+    });
+});
+
+
+server.listen(port, function(){
+    console.log('Server is up on port' + port);
 });
