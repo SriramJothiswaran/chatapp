f4b2917ae4e33df0524fc08ef4843071e7c2bef5
Sriram Jothiswaran <sriram@Srirams-MacBook-Air.local>
2017-04-16 18:55:15 +0530

    added geolocation support

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const generateMessage = require('./utils/message');
+const generateLocationMessage = require('./utils/generateLocationMessage');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,32 +18,40 @@ io.on('connection', function(socket){
     console.log("user connected");
 
     //emit from admin
-    socket.emit('newMessage',{
-        from : 'Admin',
-        text : 'Welcome to the Chat App',
-        createdAt: new Date().getTime()
-    });
+    //socket.emit('newMessage',{
+    //    from : 'Admin',
+    //    text : 'Welcome to the Chat App',
+    //    createdAt: new Date().getTime()
+    //});
+    socket.emit('newMessage', generateMessage('Admin','Welcome to the Chat App'));
 
     //from admin to new user joined
-    socket.broadcast.emit('newMessage', {
-       from : 'Admin',
-        text: 'New User Joined',
-        createdAt: new Date().getTime()
-    });
+    //socket.broadcast.emit('newMessage', {
+    //   from : 'Admin',
+    //    text: 'New User Joined',
+    //    createdAt: new Date().getTime()
+    //});
 
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New User Joined'));
+
+    //socket.on('createMessage', function(message,callback){
+    //    console.log('createMessage',message);
+    //    io.emit('newMessage',{
+    //        from:message.from,
+    //        text: message.text,
+    //        createdAt: new Date().getTime()
+    //    });
+    //    callback('From the server');
+    //
+    //});
     socket.on('createMessage', function(message,callback){
         console.log('createMessage',message);
-        io.emit('newMessage',{
-            from:message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from,message.text));
         callback('From the server');
-        //socket.broadcast.emit('newMessage',{
-        //   from: message.from,
-        //    text: message.text,
-        //    createdAt: new Date().getTime()
-        //});
+        });
+
+    socket.on('createLocationMessage', function(coords){
+       io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
 
